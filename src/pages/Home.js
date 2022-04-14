38132b1bbbd102b8f0c0c8488a59103103d3bc78
Himanshu <araj00990@gmail.com>
2022-04-14 14:20:58 +0530

    bug fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ function Home() {
         </div>
 
         <div>
-          <img alt="header-image" className="header-image" src={sticky} />
+          <img alt="Kariklodu" className="header-image" src={sticky} />
         </div>
       </div>
     </div>
